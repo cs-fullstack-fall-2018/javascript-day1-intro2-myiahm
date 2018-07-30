7a2d52204a053e200fa88aadd93d99cf9c158219
Myiah Moore <myiahsade@gmail.com>
2018-07-30 13:53:49 -0500

    check in

--- EX6.js
+++ EX6.js
@@ -1,3 +1,4 @@
 var number1 = parseInt(prompt("Enter number 1: "));
 var number2 = parseInt(prompt("Enter number 2: "));
-alert(number1+number2);
+alert("The sum is: "+ (number1+number2));
+alert("The diffrence is : "+ (number1- number2));
